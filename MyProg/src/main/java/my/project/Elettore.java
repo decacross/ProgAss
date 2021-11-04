e39ac48a12c8b22d9f6bd84c0814326ee99e6e8c
decacross <francesco.mammoliti.fm3@gmail.com>
2021-11-04 16:26:15 +0100

    Commit inserisco attributi e metodi

---
+++ MyProg/src/main/java/my/project/Elettore.java
@@ -0,0 +1,18 @@
+package my.project;
+
+public class Elettore{
+	protected String CF;
+	protected String CodiceUnivocoElettore;
+	
+	public Elettore() {
+		
+	}
+	
+	public void loginElettore(String cf, String c) {
+		
+	}
+	
+	public void selezionaVotazione () {
+		
+	}
+}
---
+++ MyProg/src/main/java/my/project/GestoreSistema.java
@@ -0,0 +1,18 @@
+package my.project;
+
+public class GestoreSistema {
+	protected String CF;
+	protected String CodiceUnivocoGestoreSistema;
+	
+	public GestoreSistema() {
+		
+	}
+	
+	public void loginGestoreSistema(String cf, String c) {
+		
+	}
+	
+	public void configuraVotazione() {
+		
+	}
+}
